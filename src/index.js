d17e75db96c138e6ae154438f45bef9c991651ce
emohitkumarpatro <mohit.patro@sprih.com>
2023-06-24 14:23:42 +0530

    removed auth dependcies

--- src/index.js
+++ src/index.js
@@ -2,20 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
+
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Auth0Provider
-    domain="dev-s1311tnwgpq5xtpk.us.auth0.com"
-    clientId="TTKOguHqUTbEGcHZRltvN2PDIE91fJX6"
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-  >
+ 
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Auth0Provider>
+ 
 );
